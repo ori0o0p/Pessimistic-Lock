a80669075fe2f911717a67f228433654976121ae
101 <tmddnjs001133@dsm.hs.kr>
2023-11-22 13:48:06 +0900

    기본 설정

---
+++ src/test/java/com/example/pessimisticlock/PessimisticLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pessimisticlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessimisticLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
