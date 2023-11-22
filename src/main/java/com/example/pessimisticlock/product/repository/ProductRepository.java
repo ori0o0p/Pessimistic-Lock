5bf4a23d77c80c61fb8e2f0b856e827a886175ae
101 <tmddnjs001133@dsm.hs.kr>
2023-11-22 15:26:38 +0900

    잠금 추가

--- src/main/java/com/example/pessimisticlock/product/repository/ProductRepository.java
+++ src/main/java/com/example/pessimisticlock/product/repository/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.example.pessimisticlock.product.repository;
 
 import com.example.pessimisticlock.product.entity.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Product> findByTitle(String title);
 }
